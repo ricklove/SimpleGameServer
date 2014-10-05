ca770214cacc76e959465e3cc0cc2b23358c4669
FaqirAamir <faamirpk@yahoo.com>
2014-10-05 11:09:32 +0500

    register tests
    more tests

--- GameServerBLL/GameServerBLL/Tests/RegisterTests.cs
+++ GameServerBLL/GameServerBLL/Tests/RegisterTests.cs
@@ -12,10 +12,29 @@ namespace GameServerBLL.Tests
     {
         ISimpleGameServer provider = SimpleGameServerProvider.Instance;
 
-        public void Register_withIvalidEmail_Fails()
+        public void Register_IvalidEmail_Fails()
         {
             string invalidEmail = "fa@ab@_solution";
             Assert.IsFalse(provider.Register(invalidEmail, "pass"));
+        }
+
+        public void Register_NoPassword_Fails()
+        {
+            string validNewEmail = "validNewEmail@yahoo.com";
+            Assert.IsFalse(provider.Register(validNewEmail, String.Empty));
+        }
+
+        public void Register_ExistingEmail_Fails()
+        {
+            string newEmail = "faamirpk@yahoo.com";
+            Assert.IsFalse(provider.Register(newEmail, "pass"));
+        }
+
+        public void Register_ValidnewEmail_AndPassword_Succeeds()
+        {
+            string newEmail = "faamirpk@yahoo.com";
+            string password = "password";
+            Assert.IsTrue(provider.Register(newEmail, password));
         }
     }
 }
